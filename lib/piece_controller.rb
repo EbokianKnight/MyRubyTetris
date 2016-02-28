612fa67e0bcc04dc495f2dda6940349dbf109baa
Adam <ebokianknight@gmail.com>
2016-02-28 11:10:20 -0500

    Condense Logic, Bugfix Collision

--- lib/piece_controller.rb
+++ lib/piece_controller.rb
@@ -1,4 +1,5 @@
 require 'io/console'
+require 'byebug'
 
 module PieceController
 
@@ -70,6 +71,42 @@ module PieceController
    return input
   end
 
+  def collision?
+    down = get_pos(MOVES[:space])
+    invalid_move?(down)
+  end
+
+  def cells_at(pos = @cursor_pos)
+    @piece.place_at(pos)
+  end
+
+  def get_pos(diff)
+    [@cursor_pos[0] + diff[0], @cursor_pos[1] + diff[1]]
+  end
+
+  def hit_other_piece?(new_pos)
+    cells = cells_at(new_pos)
+    cells.any? do |cell|
+      @board[cell].is_a?(Piece) &&
+      @board[cell] != @piece
+    end
+  end
+
+  def check_cells(new_pos)
+    cells_at(new_pos).select do |coords|
+      @board.in_bounds?(coords)
+    end
+  end
+
+  def piece_in_bounds?(new_pos)
+    cells_at(new_pos).all? { |pos| @board.in_bounds?(pos) }
+  end
+
+  def update_pos(diff)
+    new_pos = get_pos(diff)
+    @cursor_pos = new_pos if valid_move?(new_pos)
+  end
+
   def update_rotation(num)
     @piece.rotate!(num)
     unless piece_in_bounds?(@cursor_pos)
@@ -77,24 +114,25 @@ module PieceController
     end
   end
 
+  def valid_move?(new_pos)
+    piece_in_bounds?(new_pos) && !hit_other_piece?(new_pos)
+  end
+
+  def invalid_move?(new_pos)
+    !valid_move?(new_pos)
+  end
+
   def wall_kick?
-    row, col = @cursor_pos
-    if piece_in_bounds?([row,col+1])
+    right = get_pos(MOVES[:right])
+    left = get_pos(MOVES[:left])
+
+    if valid_move?(right)
       update_pos MOVES[:right]
-    elsif piece_in_bounds?([row,col-1])
+    elsif valid_move?(left)
       update_pos MOVES[:left]
     else
       nil
     end
-  end
-
-  def update_pos(diff)
-    new_pos = [@cursor_pos[0] + diff[0], @cursor_pos[1] + diff[1]]
-    @cursor_pos = new_pos if piece_in_bounds?(new_pos)
-  end
-
-  def piece_in_bounds?(new_pos)
-    @piece.place_at(new_pos).all? { |pos| @board.in_bounds?(pos) }
   end
 
 end
